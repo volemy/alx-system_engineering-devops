362d016e111a2f0f2a283d7834b520c0bbca8ba2
volemy <minayobenevolence0@gmail.com>
2024-02-19 20:01:37 +0000

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Script thats uses REST API for given employee ID, return info
+about is/her TODO list progress"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 script.py EMPLOYEE_ID")
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    url = base_url + "/" + employee_id
+
+    response = requests.get(url)
+    employee_name = response.json().get("name")
+
+    todo_url = url + "/todos"
+    response = requests.get(todo_url)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
+
+    for task in tasks:
+        if task.get("completed"):
+            done_tasks.append(task.get("title"))
+            done += 1
+
+    output = "Employee {} is done with tasks({}/{}):\n".format(
+                employee_name, done, len(tasks))
+
+    for task in done_tasks:
+        output += "\t{}\n".format(task.replace(" ", "\t "))
+
+    print(output)
