b05d62c4217cc99436674eea28f353ae536cd778
volemy <minayobenevolence0@gmail.com>
2024-03-05 14:57:56 +0000

    task 1 update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
-"""
-Function that queries Reddit Api for the first 10 posts listed
-for given subreddit
-"""
+""" function that queries the Reddit API"""
 
 import requests
 
 
 def top_ten(subreddit):
-    """
-    This method queries the reddit API and prints the first 10 hot posts
-    for given reddit
-    """
+    """Prints the first 10 hot posts on a given Reddit subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Asus-Vivobook/0.0.1'}
-    query_params = {'limit': 10}
-
-    response = requests.get(url, headers=headers, params=query_params
-             allow_redirects=False)
-
+    headers = {'User-Agent': 'VivoBook/0.0.1'}
+    params = {'limit': 10}
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        for post in data['data']['children'][:10]:
+        for post in data['data']['children']:
             print(post['data']['title'])
     else:
         print(None)
