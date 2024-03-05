30e3796b06ec73918b5b6e1edafc79bf323a0a67
volemy <minayobenevolence0@gmail.com>
2024-03-05 13:38:13 +0000

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,9 +14,12 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'Asus-Vivobook/0.0.1'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 404:
         return 0
+
     if resposnse.status_code != 200:
         raise Exception(f'Error: {response.status_code}')
+
     data = response.json()
     return data['data']['subscribers']
