c05754ce6d22aae57bd4da71a1a4828af5540730
volemy <minayobenevolence0@gmail.com>
2024-03-05 13:26:33 +0000

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Function that queries the Reddit API for a given subreddit.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    This Method returns the number od subscribers for a given subreddit
+    if invalid subreddit is given it returns 0
+    """
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'Asus-Vivobook/0.0.1'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    #if resposnse status code is 404, subreddit is not valid
+    if response.status_code == 404:
+        return 0
+
+    # if respose status code is not 200. there was an error
+    if resposnse.status_code != 200:
+        raise Exception(f'Error: {response.status_code}')
+
+    data = response.json()
+
+    # The number of subscribers stored in the 'subscribers'
+    return data['data']['subscribers']
