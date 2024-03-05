a3cff733d1d2edc87a07211964c2b48a8fb870a3
volemy <minayobenevolence0@gmail.com>
2024-03-05 19:24:06 +0000

    task 3/Advanced

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -10,29 +10,29 @@ import requests
 
 def add_title(dictionary, hot_articles):
     """
-    This method adds a list of all hot articles
+    This method adds a list of all hot articles to the dictionary
     """
-    if len(hot_articles) == 0:
+    if not hot_articles:
         return
 
     title = hot_article[0]['data']['title'].split()
     for word in title:
         for key in dictionary.keys():
-            c = re.compile("^{}$".format(key),re.I)
+            c = re.compile("^{}$".format(key), re.I)
             if c.findall(word):
                 dictionary[key] += 1
     hot_articles.pop(0)
-    add_title(dictionary, hot_posts)
+    add_title(dictionary, hot_articles)
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, after=None):
     """
     Method that queries Reddit API and parses the titlemof all hot articles
     prints a sorted count of given keywords
     """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'VivoBook/0.0.1'}
-    params = {'after': after}
+    params = {'after': after} if after else None
 
     response = requests.get(url, headers=headers params=params
                             allow_redirects=False)
@@ -41,31 +41,22 @@ def recurse(subreddit, hot_list=[]):
         return None
 
     data = response.json()
-    hot_articles = data['data']['children']
-    add_title(dictionary, hot_articles)
-    after = data['data']['after']
-    if not after:
-        return
-    recurse(subreddit, dictionary, after=after)
+    return add_title[data, data['data']['children']
 
 
-def count_words(subreddit, word_list, dictionary=None):
+def count_words(subreddit, word_list):
     """
     Instantiation
     """
-    if dictionary is none:
-        dictionary = {}
-
+    dictionary = {}
     for word in word_list:
         word = word.lower()
-
-    if word not in dictionary:
+        if word not in dictionary:
         dictionary[word] = 0
 
-    recurse(subreddit, dictionary)
+    dictionary = recurse(subreddit)
 
-    sorted_items = sorted(dictionary.items(), key=lambda kv:
-            (-kv[1], kv[0]))
+    sorted_items = sorted(dictionary.items(), key=lambda kv: (-kv[1], kv[0]))
     for item in sorted_items:
         if item[1] > 0:
             print("{}:{}".format(item[0],item[1]))
